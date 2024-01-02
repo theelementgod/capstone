01b18ff85c2b198be677e0a996bf673b8ae183bd
theelementgod <theelementgod@gmail.com>
2024-01-02 16:03:26 -0700

    working on file upload

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import CharacterSheets from "./views/CharacterSheets/CharacterSheets"
 import Data from "./views/Data/Data"
 import Home from "./views/Home/Home"
 import LogIn from "./views/LogIn/LogIn"
-import Conditions from "./views/Selections/Conditions/Conditions"
 import SignUp from "./views/SignUp/SignUp"
 import { Route, Routes } from "react-router-dom"
 
@@ -12,13 +11,9 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/login" element={<LogIn/>}/>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/signup" element={<SignUp/>}/>
-        <Route path="/charactersheets" element={<CharacterSheets/>}/>
-        <Route path="/data" element={<Data/>}/>
+
       </Routes>
-      <Conditions/>
+      <CharacterSheets/>
     </>
   )
 }
--- src/views/CharacterSheets/CharacterSheets.tsx
+++ src/views/CharacterSheets/CharacterSheets.tsx
@@ -1,11 +1,31 @@
+import { ChangeEvent, useState } from "react"
+import { storage } from "../../firebase"
+import { ref, uploadBytes } from "firebase/storage"
 import Nav from "../../components/Nav/Nav"
 
 const CharacterSheets = () => {
+  
+  const [imageUpload, setImageUpload] = useState<File | null>(null)
+  const uploadImage = () => {
+    if(imageUpload == null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name}`)
+    uploadBytes(imageRef, imageUpload).then(() => {
+      alert('Image Uploaded')
+    })
+  }
 
   return (
     <>
       <Nav/>
-      CharacterSheets
+      <input 
+      type="file" 
+      onChange={(event:ChangeEvent<HTMLInputElement>) => {
+        const imageFile = event.target.files
+        if (imageFile) {
+          setImageUpload(imageFile[0])}}
+
+        }/>
+      <button onClick={uploadImage}>Upload File</button>
     </>
   )
 }
